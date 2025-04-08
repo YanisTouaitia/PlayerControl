2413d6c19b0e0a1af6fc6dc40a9c7af890295873
Yanis Touaitia <touaitiay1@roverkids.org>
2025-04-08 14:04:20 -0400

    Version 1.2
    -Added Physics
    -Added Movement

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Move vehical forward  
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
