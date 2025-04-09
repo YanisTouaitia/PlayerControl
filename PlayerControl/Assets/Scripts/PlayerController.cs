1387ded88965eb1f5b0d49e0d33296b019df9866
Yanis Touaitia <touaitiay1@roverkids.org>
2025-04-09 13:51:10 -0400

    Verison 1.3
    -Added Locked Camera
    -Added Smoother Movement

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 20;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Move vehical forward  
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
