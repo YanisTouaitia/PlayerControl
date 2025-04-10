6d8321dafc27d30fa1cb5d096c54b274fbd018b9
Yanis Touaitia <touaitiay1@roverkids.org>
2025-04-10 13:24:46 -0400

    Version 1.4 (Finished)
    -Added smoother turning movement

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 20;
+    private float turnSpeed = 40;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +16,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
-
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        //Moves the car based on Vertical Input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //turns the car based on Horizontal Input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
